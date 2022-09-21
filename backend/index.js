acd17e09dfdf1ea1d8e5197fbc49b9747fbfc940
Asaf M <302302302v@gmail.com>
2022-09-21 15:35:41 +0300

    initial authentication code

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,37 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-var cors = require('cors');
-const indexRouter = require('./router.js');
+const cors = require('cors');
 app.use(cors());
+const indexRouter = require('./router.js');
+const MediaWikiStrategy = require('passport-mediawiki-oauth').OAuthStrategy;
+const passport = require('passport');
 
+passport.use(new MediaWikiStrategy({
+    consumerKey: process.env.consumerKey,
+    consumerSecret: process.env.consumerSecret,
+    callbackURL: "localhost:3000"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate({ mediawikiGlobalId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/mediawiki',
+  passport.authenticate('mediawiki', { scope: 'session' }));
+ 
+app.get('/auth/mediawiki/callback', 
+  passport.authenticate('mediawiki', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 app.use(bodyParser.json());
 
