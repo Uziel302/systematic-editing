131994e49e11ae0450ad521a04e86bb938e0c612
Asaf M <302302302v@gmail.com>
2022-09-25 19:35:40 +0300

    move stuff to router

--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const MediaWikiStrategy = require("passport-mediawiki-oauth").OAuthStrategy;
+const passport = require("passport");
+passport.use(
+  new MediaWikiStrategy(
+    {
+      consumerKey: process.env.consumerKey,
+      consumerSecret: process.env.consumerSecret,
+      callbackURL: "https://ctioc.org/api/auth/mediawiki/callback",
+    },
+    function (token, tokenSecret, profile, done) {
+      profile.oauth = {
+        consumer_key: process.env.consumerKey,
+        consumer_secret: process.env.consumerSecret,
+        token: token,
+        token_secret: tokenSecret,
+      };
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+router.use(passport.initialize());
+router.use(passport.session());
+
 const { getTypos, replaceTypo } = require("./controllers/typos.controller");
 
 router.get("/typos", (req, res) => {
@@ -9,4 +40,18 @@ router.post("/replaceTypo",
 (req, res) => {
   return replaceTypo(req, res);
 });
+
+router.get(
+  "/auth/mediawiki",
+  passport.authenticate("mediawiki", { scope: "session" })
+);
+
+router.get(
+  "/auth/mediawiki/callback",
+  passport.authenticate("mediawiki", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect((process.env.front??'') + "success/" + req.user.displayName);
+  }
+);
 module.exports = router;
