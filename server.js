33f76b2dc6d894f46cc8e2d9a5a572a98f1747d8
Asaf M <302302302v@gmail.com>
2022-10-15 13:58:46 +0300

    move auth part back to main script

--- server.js
+++ server.js
@@ -4,15 +4,60 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(cors());
 const indexRouter = require("./router.js");
-const authRouter = require("./auth.js");
 const port = parseInt(process.env.PORT ?? 3000, 10); 
+const session = require("express-session");
 
-app.use(
-  require("express-session")({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-  })
+const crendentials = require('./credential.json');
+const passport = require("passport");
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+app.use( session( {
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 30
+  },
+  secret: crendentials.consumer_secret
+} ) );
+app.use(passport.initialize());
+app.use(passport.session());
+const MediaWikiStrategy = require("passport-mediawiki-oauth").OAuthStrategy;
+passport.use(
+  new MediaWikiStrategy(
+    {
+      consumerKey: crendentials.consumer_key,
+      consumerSecret: crendentials.consumer_secret,
+      callbackURL: "https://typos.toolforge.org/auth/mediawiki/callback",
+    },
+    function (token, tokenSecret, profile, done) {
+      profile.oauth = {
+        consumer_key: crendentials.consumer_key,
+        consumer_secret: crendentials.consumer_secret,
+        token: token,
+        token_secret: tokenSecret,
+      };
+      return done(null, profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/mediawiki",
+  passport.authenticate("mediawiki", { scope: "session" })
+);
+
+app.get(
+  "/auth/mediawiki/callback",
+  passport.authenticate("mediawiki", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect((crendentials.front ?? ""));
+  }
 );
 
 app.use(bodyParser.json());
@@ -22,7 +67,6 @@ app.use(
   })
 );
 
-app.use("/auth", authRouter);
 app.use("/api", indexRouter);
 
 app.use(express.static('angular'));
