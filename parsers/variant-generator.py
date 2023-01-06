790cedb020ceb493adaa95a17764e71b10a08d14
Asaf Malin <asafmalin@gmail.com>
2023-01-07 00:09:32 +0200

    use word frequencies to create variants

---
+++ parsers/variant-generator.py
@@ -0,0 +1,53 @@
+#list of words by frequency thanks to https://github.com/IlyaSemenov/wikipedia-word-frequency/
+#the file is too big for browsers, got the file by running wget https://raw.githubusercontent.com/IlyaSemenov/wikipedia-word-frequency/master/results/enwiki-2022-08-29.txt
+with open('enwiki-word-by-frequency.txt') as f:
+   words = {}
+   for line in f:
+      (key, val) = line.split()
+      words[key] = int(val)
+
+def getVariations(word):
+    omit = {}
+    double = {}
+    swap = {}
+    swap2 = {}
+    replace = {}
+    #duplicating a letter
+    for x in range(len(word)):
+        double[(word[0 : x : ] + word[x] + word[x : :])] = x
+    #omiting a letter
+    for x in range(len(word)):
+        omit[(word[0 : x : ] + word[x + 1 : :])] = x
+    #swapping a letter
+    for x in range(len(word)-1):
+        swap[(word[0 : x : ] + word[x + 1] + word[x] + word[x + 2 : :])] = x
+    #swapping a 2nd letter
+    for x in range(len(word)-2):
+        swap2[(word[0 : x : ] + word[x + 2]  + word[x + 1] + word[x] + word[x + 3 : :])] = x
+    #replacing by another letter
+    for letter in 'aeiou':
+        for x in range(len(word)):
+            if word[x] in 'aeiou':
+                replace[(word[0 : x : ] + letter + word[x + 1 : :])] = x
+    variations = {
+        'double': double,
+        'omit': omit,
+        'swap': swap,
+        'swap2': swap2,
+        'replace': replace
+    }
+    return variations
+
+f = open('variations.txt', 'w')
+
+for word in words:
+   if words[word]<400:
+      break
+   wordVariations = getVariations(word)
+   for variationType in wordVariations:
+      for variant in wordVariations[variationType]:
+         if variant not in words and len(variant)>0:
+            spot = wordVariations[variationType][variant]
+            combined = variant+','+word+','+variationType+','+str(spot)+'\n'
+            f.write(combined)
+               
